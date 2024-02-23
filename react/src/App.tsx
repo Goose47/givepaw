d4a31d48d8cb90241c9ea0822b96cf283bfbd38a
artem <uvuv.space@gmail.com>
2024-02-23 20:21:42 +0300

    fixed api url

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -7,7 +7,7 @@ import Login from './pages/Login';
 import { ConfigProvider } from 'antd';
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.uvuv643.ru"
+axios.defaults.baseURL = "https://dev.api.uvuv643.ru"
 
 function App() {
   const router = createBrowserRouter([
