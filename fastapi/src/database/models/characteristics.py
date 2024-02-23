fe8864b1c6c09fe2c7ee8c41b9689619041711d4
CHB-0r1s <borisluzin2004@gmail.com>
2024-02-23 14:08:34 +0300

    Add PetVaccination template

--- fastapi/src/database/models/characteristics.py
+++ fastapi/src/database/models/characteristics.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from base import Base
 
@@ -62,20 +63,14 @@ class Breed(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
     title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
 
-<<<<<<< HEAD
-
-=======
     pet_type_id: Mapped[int] = mapped_column(ForeignKey("pet_types.id"), nullable=False)
 
-    
->>>>>>> a1df19adc09652f17f13b1339ef44896915ec56f
+
 class Region(Base):
     __tablename__ = "regions"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
     title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-<<<<<<< HEAD
-=======
 
 
 class UsersConfig(Base):
@@ -114,4 +109,3 @@ class PetVaccination(Base):
 
 # TODO: Проверить что нет лишнего unique
 # TODO: Перенести модельки в ассоциативные
->>>>>>> a1df19adc09652f17f13b1339ef44896915ec56f
