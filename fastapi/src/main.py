f5c77dfe8bd810546830561a8977fdf71e56c0c1
ITSamantha <diana.saveleva.dos@gmail.com>
2024-02-24 00:31:21 +0300

    commit

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -21,11 +21,11 @@ def get_application() -> FastAPI:
 
 app = get_application()
 
-
+"""
 @app.exception_handler(AppValidationException)
 async def validation_failed(request: Request, exc: AppValidationException):
     return ApiResponse.errors(exc.errors, status_code=422)
-
+"""
 
 if __name__ == "__main__":
     uvicorn.run(
