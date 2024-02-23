df80faced7938f0373d4f0ebbef5407c358f5abb
CHB-0r1s <borisluzin2004@gmail.com>
2024-02-23 16:12:47 +0300

    Add mock get route for PetBloodGroup

--- fastapi/src/api/routers/mock_routes.py
+++ fastapi/src/api/routers/mock_routes.py
@@ -26,18 +26,16 @@ class MockBloodGroup(BaseModel):
 
 class MockPetBloodGroupSchema(BaseModel):
     id: int
-    mock_blood_group: MockBloodGroup
-    mock_pet_type: MockPetType
-    mock_rhesus_type: MockRhesusType
+    blood_group: MockBloodGroup
+    rhesus_type: MockRhesusType
 
 
 @router.get("/pet_blood_group/{animal_type}", response_model=list[MockPetBloodGroupSchema])
 async def mock_blood_group(request: Request, animal_type: str):
     return [MockPetBloodGroupSchema(
         id=random.randint(1, 100),
-        mock_pet_type=MockPetType(id=random.randint(1, 100), title=animal_type),
-        mock_blood_group=MockBloodGroup(id=random.randint(1, 100), title=random.choice(["I", "II", "III"])),
-        mock_rhesus_type=MockRhesusType(id=random.randint(1, 100), title=random.choice([
+        blood_group=MockBloodGroup(id=random.randint(1, 100), title=random.choice(["I", "II", "III"])),
+        rhesus_type=MockRhesusType(id=random.randint(1, 100), title=random.choice([
             f"фактор {animal_type} 1",
             f"фактор {animal_type} 2",
             f"фактор {animal_type} 3"
