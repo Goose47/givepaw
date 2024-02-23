0bbda466677e8b1ac44e72506e5b371ef5a35ec4
CHB-0r1s <borisluzin2004@gmail.com>
2024-02-23 19:10:27 +0300

    Add new cities rout

--- fastapi/src/api/routers/pets.py
+++ fastapi/src/api/routers/pets.py
@@ -1,11 +1,13 @@
 from http import HTTPStatus
-from typing import List
+from typing import List, Dict
 
 from fastapi import APIRouter, HTTPException
+
 from src.database.session_manager import db_manager
 from src.repository.crud.base_crud_repository import SqlAlchemyRepository
 from src.schemas import characteristics as schemas
 from src.database.models import characteristics as models
+from src.schemas.characteristics import PetType
 
 router = APIRouter(
     prefix="/pets",
@@ -19,6 +21,9 @@ async def get_pet_types():
         types: List[models.PetType] = await SqlAlchemyRepository(db_manager.get_session,
                                                                  model=models.PetType).get_multi()
 
-        return [schemas.PetType(id=t.id, title=t.title) for t in types]
+        return [schemas.PetType(id=t.id,
+                                title=t.title,
+                                icon=t.icon) for t in types]
+
     except Exception as e:
-        raise HTTPException(status_code=HTTPStatus.IM_A_TEAPOT, detail={"couse": "Artem"})
+        raise HTTPException(status_code=HTTPStatus.IM_A_TEAPOT, detail={"cause": "Artem"})
