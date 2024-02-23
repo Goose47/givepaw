83f26e6ac08e23f51e3c16c7ab23406c98cfcdd9
Dead <space.gee.z@gmail.com>
2024-02-23 13:56:47 +0300

    format all files

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RouterWrapper from "./RouterWrapper";
 import Menu from "./components/Header/Header";
-import Home from './components/Home/Home'
+import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 
-export const APP_URL = 'http://localhost:3000/'
+export const APP_URL = "http://localhost:3000/";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <RouterWrapper>
+          <Home />
+        </RouterWrapper>
+      ),
+    },
+    {
+      path: "/sign-in",
+      element: (
+        <RouterWrapper>
+          <Login />
+        </RouterWrapper>
+      ),
+    },
+  ]);
 
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <RouterWrapper><Home /></RouterWrapper>,
-        },
-        {
-            path: "/sign-in",
-            element: <RouterWrapper><Login /></RouterWrapper>,
-        },
-    ]);
-
-    return (
-        <>
-            <React.StrictMode>
-                <RouterProvider router={router} />
-            </React.StrictMode>
-        </>
-
-    )
+  return (
+    <>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </>
+  );
 }
 
-export default App
+export default App;
--- react/src/RouterWrapper.tsx
+++ react/src/RouterWrapper.tsx
@@ -1,25 +1,19 @@
-import React, {ReactNode} from 'react'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import React, { ReactNode } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 interface RouterWrapperProps {
-    children : ReactNode
+  children: ReactNode;
 }
 
-function RouterWrapper(props : RouterWrapperProps) {
-
-    return (
-        <>
-            <Header/>
-            <div className="container">
-                { props.children }
-            </div>
-            <Footer/>
-        </>
-        
-
-    )
-
+function RouterWrapper(props: RouterWrapperProps) {
+  return (
+    <>
+      <Header />
+      <div className="container">{props.children}</div>
+      <Footer />
+    </>
+  );
 }
 
-export default RouterWrapper
+export default RouterWrapper;
