829d5e8d3e40d07ca204726d90d63707acd6c10f
Dead <space.gee.z@gmail.com>
2024-02-23 15:22:31 +0300

    add forms base

--- react/src/pages/DonorForm.tsx
+++ react/src/pages/DonorForm.tsx
@@ -1,5 +1,70 @@
+import { useState } from "react";
+import Field from "../components/global/dropdown/Field";
+
 const DonorForm = () => {
-  return <div></div>;
+  const [animalType, setAnimalType] = useState("");
+  const [breed, setBreed] = useState("");
+  const [bloodGroup, setBloodGroup] = useState("");
+  const [petName, setPetName] = useState("");
+  const [image, setImage] = useState(undefined);
+  const [age, setAge] = useState("");
+  const [weight, setWeight] = useState("");
+  const [vaccinations, setVaccinations] = useState([]);
+
+  return (
+    <div>
+      <div>Форма донора</div>
+      <div>
+        <Field
+          label={"Тип животного"}
+          id={"animalType"}
+          type="text"
+          value={animalType}
+          setValue={setAnimalType}
+        />
+        <Field
+          label={"Порода"}
+          id={"breed"}
+          type="text"
+          value={breed}
+          setValue={setBreed}
+        />
+        <Field
+          label={"Группа крови"}
+          id={"bloodGroup"}
+          type="number"
+          value={bloodGroup}
+          setValue={setBloodGroup}
+        />
+        <div>Загрузите фото животного</div>
+        <input type="file" accept="image/png, image/jpeg"></input>
+        <Field
+          label={"Кличка"}
+          id={"petName"}
+          type="text"
+          value={petName}
+          setValue={setPetName}
+        />
+        <Field
+          label={"Возраст"}
+          id={"age"}
+          type="number"
+          value={age}
+          setValue={setAge}
+        />
+        <Field
+          label={"Вес"}
+          id={"weight"}
+          type="number"
+          value={weight}
+          setValue={setWeight}
+        />
+        {/* <Field label={"Прививки"} id={"vaccinations"} value={vaccinations} setValue={setVaccinations}/> */}
+
+        <button>Отправить заявку</button>
+      </div>
+    </div>
+  );
 };
 
 export default DonorForm;
--- react/src/pages/RecipientForm.tsx
+++ react/src/pages/RecipientForm.tsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import Field from "../components/global/dropdown/Field";
+
 const RecipientForm = () => {
-  return <div></div>;
+  const [reason, setReason] = useState("");
+  const [veterinaryСlinic, setVeterinaryСlinic] = useState("");
+  const [bloodAmount, setBloodAmount] = useState("");
+  const [deadline, setDeadline] = useState("");
+
+  return (
+    <div>
+      <div>Форма реципиента</div>
+      <div>
+        <Field
+          label={"Причина поиска животного-донора"}
+          id={"reason"}
+          type="text"
+          value={reason}
+          setValue={setReason}
+        />
+        <Field
+          label={"Ветеринарная клиника"}
+          id={"veterinaryСlinic"}
+          type="text"
+          value={veterinaryСlinic}
+          setValue={setVeterinaryСlinic}
+        />
+        <Field
+          label={"Количество крови"}
+          id={"bloodAmount"}
+          type="number"
+          value={bloodAmount}
+          setValue={setBloodAmount}
+        />
+        <Field
+          label={"Дата, до которой поиск актуален"}
+          id={"deadline"}
+          type="date"
+          value={deadline}
+          setValue={setDeadline}
+        />
+
+        <button>Отправить заявку</button>
+      </div>
+    </div>
+  );
 };
 
 export default RecipientForm;
