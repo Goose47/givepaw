0dff797702cf129a9ab2351ba2b0c650c713e493
CHB-0r1s <borisluzin2004@gmail.com>
2024-02-23 14:12:05 +0300

    Replace Associative models

--- fastapi/src/database/models/associacive.py
+++ fastapi/src/database/models/associacive.py
@@ -28,6 +28,15 @@ class User(Base):
     avatar_id: Mapped[int] = mapped_column(ForeignKey("avatars.id"), nullable=False)
 
 
+class City(Base):
+    __tablename__ = "cities"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+
+    region_id: Mapped[int] = mapped_column(ForeignKey("regions.id"), nullable=False)
+
+
 class Pet(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
     pet_blood_group_id: Mapped[int] = mapped_column(ForeignKey("pet_blood_groups.id"), nullable=False)
@@ -36,3 +45,37 @@ class Pet(Base):
     avatar_id: Mapped[int] = mapped_column(ForeignKey("avatars.id"), nullable=False)
     name: Mapped[str] = mapped_column(String, nullable=False)
     age: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+class PetBloodGroup(Base):
+    __tablename__ = "pet_blood_groups"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    blood_group: Mapped[str] = mapped_column(String, index=True, nullable=False)
+
+    pet_type_id: Mapped[int] = mapped_column(ForeignKey("pet_types.id"), nullable=False)
+    rhesus_id: Mapped[int] = mapped_column(ForeignKey("rhesus.id"), nullable=False)
+
+
+class UsersConfig(Base):
+    __tablename__ = "users_configs"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    phone_number_status: Mapped[int] = mapped_column(Integer, index=True, nullable=False)
+    social_networks_status: Mapped[int] = mapped_column(Integer, index=True, nullable=False)
+    email_status: Mapped[int] = mapped_column(Integer, index=True, nullable=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+
+class Breed(Base):
+    __tablename__ = "breeds"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+
+    pet_type_id: Mapped[int] = mapped_column(ForeignKey("pet_types.id"), nullable=False)
+
+
+class PetVaccination(Base):
+    __table__ = "pet_vaccination"
--- fastapi/src/database/models/characteristics.py
+++ fastapi/src/database/models/characteristics.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from base import Base
@@ -34,15 +34,6 @@ class Vaccination(Base):
     title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
 
 
-class City(Base):
-    __tablename__ = "cities"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-
-    region_id: Mapped[int] = mapped_column(ForeignKey("regions.id"), nullable=False)
-
-
 class ReasonSearch(Base):
     __tablename__ = "reasons_search"
 
@@ -57,31 +48,11 @@ class Rhesus(Base):
     title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
 
 
-class Breed(Base):
-    __tablename__ = "breeds"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-
-    pet_type_id: Mapped[int] = mapped_column(ForeignKey("pet_types.id"), nullable=False)
-
-
 class Region(Base):
     __tablename__ = "regions"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
     title: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-
-
-class UsersConfig(Base):
-    __tablename__ = "users_configs"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    phone_number_status: Mapped[int] = mapped_column(Integer, index=True, nullable=False)
-    social_networks_status: Mapped[int] = mapped_column(Integer, index=True, nullable=False)
-    email_status: Mapped[int] = mapped_column(Integer, index=True, nullable=False)
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
 
 
 class Avatar(Base):
@@ -93,19 +64,5 @@ class Avatar(Base):
     photo_thumb: Mapped[str] = mapped_column(String, index=True, nullable=True)
 
 
-class PetBloodGroup(Base):
-    __tablename__ = "pet_blood_groups"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    blood_group: Mapped[str] = mapped_column(String, index=True, nullable=False)
-
-    pet_type_id: Mapped[int] = mapped_column(ForeignKey("pet_types.id"), nullable=False)
-    rhesus_id: Mapped[int] = mapped_column(ForeignKey("rhesus.id"), nullable=False)
-
-
-class PetVaccination(Base):
-    __table__ = "pet_vaccination"
-
 
 # TODO: Проверить что нет лишнего unique
-# TODO: Перенести модельки в ассоциативные
