2f2d738298cba4964f8c53ed630cf7bb1eebb56b
CHB-0r1s <borisluzin2004@gmail.com>
2024-02-23 19:08:20 +0300

    Add new pydantic models for City

--- fastapi/src/schemas/characteristics.py
+++ fastapi/src/schemas/characteristics.py
@@ -4,3 +4,16 @@ from pydantic import BaseModel
 class PetType(BaseModel):
     id: int
     title: str
+    icon: str
+
+
+class Region(BaseModel):
+    id: int
+    title: str
+
+
+class City(BaseModel):
+    id: int
+    title: str
+    region: Region
+
