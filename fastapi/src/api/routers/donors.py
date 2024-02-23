91decde9fc1db78c1a5f55c0fa267c14206e4a6e
ITSamantha <diana.saveleva.dos@gmail.com>
2024-02-24 00:29:35 +0300

    commit

--- fastapi/src/api/routers/donors.py
+++ fastapi/src/api/routers/donors.py
@@ -7,6 +7,7 @@ from src.database.session_manager import db_manager
 from src.repository.crud.base_crud_repository import SqlAlchemyRepository
 from src.schemas import donors
 from src.database import models
+from src.schemas.donors import Donor
 
 router = APIRouter(
     prefix="/donors",
@@ -14,7 +15,7 @@ router = APIRouter(
 )
 
 
-@router.get('/', response_model=List[models.Donor])
+@router.get('/', response_model=List[donors.Donor])
 async def index():
     try:
         donors: List[models.Donor] = await SqlAlchemyRepository(db_manager.get_session, model=Donor).get_multi()
@@ -24,10 +25,10 @@ async def index():
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=str(e))
 
 
-@router.post('/', response_model=models.Donor)
+@router.post('/', response_model=donors.Donor)
 async def store(data: donors.DonorCreate):
     try:
-        donor: models.Donor = await SqlAlchemyRepository(db_manager.get_session, model=Donor).create(data)
+        donor: models.Donor = await SqlAlchemyRepository(db_manager.get_session, model=donors.Donor).create(data)
         return donor
 
     except Exception as e:
