2ca9394036f51e702fcb9e4e45e855333fde6fb8
Dead <space.gee.z@gmail.com>
2024-02-23 14:05:07 +0300

    add forms

---
+++ react/src/components/Forms/DonorForm/DonorForm.tsx
@@ -0,0 +1,5 @@
+const DonorForm = () => {
+  return <div></div>;
+};
+
+export default DonorForm;
---
+++ react/src/components/Forms/RecipientForm/RecipientForm.tsx
@@ -0,0 +1,5 @@
+const RecipientForm = () => {
+  return <div></div>;
+};
+
+export default RecipientForm;
