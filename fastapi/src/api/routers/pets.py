fd08311fa76d878a514c2516f127e7d959c30389
ITSamantha <diana.saveleva.dos@gmail.com>
2024-02-23 20:43:28 +0300

    commit

--- fastapi/src/api/routers/pets.py
+++ fastapi/src/api/routers/pets.py
@@ -5,7 +5,9 @@ from fastapi import APIRouter, HTTPException
 
 from src.database.session_manager import db_manager
 from src.repository.crud.base_crud_repository import SqlAlchemyRepository
-from src.schemas import pet_type
+
+from src.schemas import pet_type, vaccination, breed
+from src.database import models
 
 router = APIRouter(
     prefix="/pets",
@@ -13,41 +15,41 @@ router = APIRouter(
 )
 
 
-@router.get('/pet_types', response_model=List[PetType])
+@router.get('/pet_types', response_model=List[pet_type.PetType])
 async def get_pet_types():
     try:
-        types: List[PetType] = await SqlAlchemyRepository(db_manager.get_session,
+        types: List[models.PetType] = await SqlAlchemyRepository(db_manager.get_session,
                                                                  model=models.PetType).get_multi()
 
-        return [schemas.PetType(id=t.id,
-                                title=t.title,
-                                icon=t.icon) for t in types]
+        return [pet_type.PetType(id=t.id,
+                                 title=t.title,
+                                 icon=t.icon) for t in types]
 
     except Exception as e:
         raise HTTPException(status_code=HTTPStatus.IM_A_TEAPOT, detail={"cause": "Artem"})
 
 
-@router.get('/vaccinations', response_model=List[schemas.Vaccination])
+@router.get('/vaccinations', response_model=List[vaccination.Vaccination])
 async def get_vaccinations():
     try:
         vaccinations: List[models.Vaccination] = await SqlAlchemyRepository(db_manager.get_session,
                                                                             model=models.Vaccination).get_multi()
 
-        return [schemas.Vaccination(id=v.id,
-                                    title=v.title) for v in vaccinations]
+        return [vaccination.Vaccination(id=v.id,
+                                        title=v.title) for v in vaccinations]
 
     except Exception as e:
         raise HTTPException(status_code=HTTPStatus.IM_A_TEAPOT, detail={"cause": "Artem"})
 
 
-@router.get('/breeds', response_model=List[schemas.Breed])
+@router.get('/breeds', response_model=List[breed.Breed])
 async def get_breeds():
     try:
         breeds: List[models.Breed] = await SqlAlchemyRepository(db_manager.get_session,
                                                                 model=models.Breed).get_multi()
 
-        return [schemas.Breed(id=v.id,
-                              title=v.title) for v in breeds]
+        return [breed.Breed(id=v.id,
+                            title=v.title) for v in breeds]
 
     except Exception as e:
         raise HTTPException(status_code=HTTPStatus.IM_A_TEAPOT, detail={"cause": "Artem"})
