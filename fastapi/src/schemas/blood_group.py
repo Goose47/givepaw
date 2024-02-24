9884a0a787fbb715018eac6b0f669dc0de76b5f5
ITSamantha <diana.saveleva.dos@gmail.com>
2024-02-24 12:10:39 +0300

    commit

--- fastapi/src/schemas/blood_group.py
+++ fastapi/src/schemas/blood_group.py
@@ -27,7 +27,7 @@ class PetBloodGroup(BaseModel):
 
 def create_pet_blood_group(pet_blood_group):
     return PetBloodGroup(id=pet_blood_group.id, blood_group=create_blood_group(pet_blood_group.blood_group),
-                         rhesus=create_rhesus_type(pet_blood_group.rhesus_type))
+                         rhesus=create_rhesus_type(pet_blood_group.rhesus))
 
 
 class BloodComponent(BaseModel):
