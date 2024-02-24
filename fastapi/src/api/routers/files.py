c1462653fef611b228f024530d9e29421beb0386
Nikita <tyumin000@gmail.com>
2024-02-24 13:22:17 +0300

    yoyoyoy test

--- fastapi/src/api/routers/files.py
+++ fastapi/src/api/routers/files.py
@@ -18,3 +18,12 @@ async def get_file(path: str):
         return FileResponse(image_path)
     except Exception as e:
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail={"cause": e.with_traceback})
+
+
+@router.get('/avatars/{path}')
+async def get_file(path: str):
+    try:
+        image_path = settings_app.APP_PATH + '/storage/avatars/' + path
+        return FileResponse(image_path)
+    except Exception as e:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail={"cause": e.with_traceback})
