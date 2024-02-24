63a1c9ceac2e210df78451598f7e74f021eda17c
ITSamantha <diana.saveleva.dos@gmail.com>
2024-02-24 10:34:53 +0300

    commit

--- fastapi/src/api/routers/recipients.py
+++ fastapi/src/api/routers/recipients.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, HTTPException
 
 from src.database.session_manager import db_manager
 from src.repository.crud.base_crud_repository import SqlAlchemyRepository
-from src.schemas.recipients import RecipientViewType, RecipientCreateType
+from src.schemas import recipients
 from src.database.models.associative import Recipient
 
 router = APIRouter(
@@ -14,7 +14,7 @@ router = APIRouter(
 )
 
 
-@router.get('/', response_model=List[RecipientViewType])
+@router.get('/', response_model=List[recipients.Recipient])
 async def index():
     try:
         recipients: List[Recipient] = await SqlAlchemyRepository(db_manager.get_session, model=Recipient).get_multi()
@@ -24,8 +24,8 @@ async def index():
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=str(e))
 
 
-@router.post('/', response_model=RecipientViewType)
-async def store(data: RecipientCreateType):
+@router.post('/', response_model=recipients.Recipient)
+async def store(data: recipients.RecipientCreate):
     try:
         recipient: Recipient = await SqlAlchemyRepository(db_manager.get_session, model=Recipient).create(data)
         return recipient
