dc96066ee7a24b810363487556d9bcf4402af24d
Dead <space.gee.z@gmail.com>
2024-02-24 13:37:11 +0300

    add login button change

--- react/src/components/global/Header.tsx
+++ react/src/components/global/Header.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import { BsGeoAltFill } from 'react-icons/bs';
 import CitySelect from './CitySelect';
+import { useDispatch, useSelector} from 'react-redux';
+import { fetchUser, selectUser } from '../../redux/slices/UserSlice';
 
 const Header = () => {
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser() as any)
+  }, [dispatch])
+
   return (
     <header className="Header">
       <div className="container">
@@ -23,9 +32,14 @@ const Header = () => {
               <BsGeoAltFill />
               <span className="Select__Placeholder__Title">Ваш регион</span>
             </CitySelect>
-            <Link to={'/login'}>
-              <Button type="primary">Войти</Button>
+            {user ?  
+            <Link to={'/profile'}>
+              <Button type="primary">Мой профиль</Button>
             </Link>
+            :  <Link to={'/login'}>
+              <Button type="primary">Войти</Button>
+            </Link>}
+           
           </div>
         </div>
       </div>
