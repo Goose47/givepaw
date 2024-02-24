d037b28c5af153c099583e6ef318442bcd240e6a
Nikita <tyumin000@gmail.com>
2024-02-25 00:27:28 +0300

    bases

--- fastapi/src/database/models/associative.py
+++ fastapi/src/database/models/associative.py
@@ -73,7 +73,8 @@ class Pet(Base):
     pet_type: Mapped["PetType"] = relationship(uselist=False, lazy="selectin")
 
     avatar_id: Mapped[int] = mapped_column(ForeignKey("avatars.id"), nullable=True)
-    avatar: Mapped["Avatar"] = relationship(uselist=False, lazy="selectin")@property
+    avatar: Mapped["Avatar"] = relationship(uselist=False, lazy="selectin")
+    @property
     def avatar_link(self):
         return (settings_app.APP_URL + '/files/avatars/' + self.avatar.photo_path) if self.avatar_id else None
 
