d27af2f353f1f7d0ebcf7572bbcf0c3b0591fad5
artem <uvuv.space@gmail.com>
2024-02-24 14:13:16 +0300

    fixed profile markup

--- react/src/pages/Profile.tsx
+++ react/src/pages/Profile.tsx
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUser, selectUser } from '../redux/slices/UserSlice';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { fetchPets, selectPets } from '../redux/slices/PetsSlice';
+import PetItem, { Pet } from "../components/global/PetItem";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -15,25 +17,34 @@ const Profile = () => {
 
   return (
     user && (
-      <>
-        <div>
-          {/* todo: add photo */}
-          <div>{user.username}</div>
-          <div>ФИО: {user.name + ' ' + user.surname + ' ' + user.patronymic}</div>
-          <div>Email: {user.email}</div>
-        </div>
-        <div>
-          {pets.map((pet: any) => (
-            <div key={pet.id}>
-              {/* <img src={pet.avatar.photo_thumb} alt={pet.name} /> */}
-              <div>{pet.name}</div>
-              <div>Возраст: {pet.age}</div>
-              <div>Вес: {pet.weight}</div>
-              <div>Порода: {pet.breed}</div>
+      <div className="Profile">
+        <h1>Мой профиль</h1>
+        <div className="Profile__Info">
+          <div className="Profile__Header">
+            <div className="Profile__Header__Avatar">
+              <img src="https://http.cat/502" alt="#" />
             </div>
-          ))}
+            <h2>{user.username}</h2>
+          </div>
+          <div>{user.name + ' ' + user.surname + ' ' + user.patronymic}</div>
+          <div>{user.email}</div>
         </div>
-      </>
+        {
+          pets.length > 0 ? (
+            <div className="Profile__Pets">
+              <h3>Ваши питомцы: </h3>
+              <div className="Profile__Row">
+                {pets.map((pet: Pet) => (
+                  <div className="Profile__Item"><PetItem pet={pet} /></div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <h4>Вы можете <Link to="/donor-form">записать питомца на донорство крови</Link> или <Link to="/recipient-form">оставить заявку, если он нуждается в этом</Link></h4>
+          )
+        }
+
+      </div>
     )
   );
 };
