c57fe12e179d2f5469b02e82bd9837775fa68461
Nikita <tyumin000@gmail.com>
2024-02-24 23:48:28 +0300

    base64 for user images not tested

--- fastapi/src/api/routers/auth.py
+++ fastapi/src/api/routers/auth.py
@@ -13,9 +13,9 @@ router = APIRouter(
 
 
 @router.post("/register")
-async def register(user: RegisterUser = Depends(RegisterUser.as_form), avatar: UploadFile = File()):
+async def register(user: RegisterUser):
     try:
-        registered_user: UserType = await RegisterUseCase.register(user, avatar)
+        registered_user: UserType = await RegisterUseCase.register(user)
         access_token, refresh_token, user = await LoginUseCase.login(
             LoginUser(username=user.username, password=user.password))
     except Exception as e:
