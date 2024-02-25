509653a0ffe64538c26081a981be4b14b9d6f32f
artem <uvuv.space@gmail.com>
2024-02-25 09:33:33 +0300

    fixed put to post

--- react/src/service/data.service.tsx
+++ react/src/service/data.service.tsx
@@ -68,7 +68,7 @@ export const getMostNecessary = async () => {
 
 export const editUser = async (surname: string, name: string, patronymic: string, username: string, email: string, city_id: string) => {
   try {
-    const response = await axios.put('users/user/', {surname, name, patronymic,username, email, city_id });
+    const response = await axios.post('users/user/', {surname, name, patronymic,username, email, city_id });
     return response.data;
   } catch (error) {
     console.error(error);
