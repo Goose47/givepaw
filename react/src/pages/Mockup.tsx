58b42d884c03075d4cb8da028ea7c5db75956de2
Dead <space.gee.z@gmail.com>
2024-02-25 09:57:58 +0300

    fix image sizw

--- react/src/pages/Mockup.tsx
+++ react/src/pages/Mockup.tsx
@@ -1,12 +1,16 @@
 const Mockup = () => {
   return (
     <div>
-      <h1> Этой страницы пока что не существует, зато у нас есть милый котик! </h1>
+      <h3> Этой страницы пока что не существует, зато у нас есть милый котик! </h3>
       <div style={{
                           display: 'flex',
                           justifyContent: 'center'
                         }} >
-<img  src={"https://dev.api.uvuv643.ru/files/assets/cat.png"} alt="mockup"  />
+<img  src={"https://dev.api.uvuv643.ru/files/assets/cat.png"} style={{
+                          width: 200,
+                          height: 200,
+                          marginRight: 5,
+                        }}  alt="mockup"  />
       </div>
       
     </div>
