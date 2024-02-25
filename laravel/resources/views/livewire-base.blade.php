e46a243def009bc4d78884507cd23a3a197f9ea6
artem <uvuv.space@gmail.com>
2024-02-25 06:56:33 +0300

    FIXED package

--- laravel/resources/views/livewire-base.blade.php
+++ laravel/resources/views/livewire-base.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   @livewireStyles
-
+<script src="https://cdn.tailwindcss.com"></script>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
