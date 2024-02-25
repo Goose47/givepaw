2b539a45b1d8528e97db6f236666616a643a3744
Nikita <tyumin000@gmail.com>
2024-02-25 03:39:20 +0300

    zalupa

--- laravel/app/Tables/PetsTable.php
+++ laravel/app/Tables/PetsTable.php
@@ -5,6 +5,7 @@ namespace App\Tables;
 use App\Models\Pet;
 use Okipa\LaravelTable\Abstracts\AbstractTableConfiguration;
 use Okipa\LaravelTable\Table;
+use Okipa\LaravelTable\Column;
 
 class PetsTable extends AbstractTableConfiguration
 {
@@ -16,7 +17,16 @@ class PetsTable extends AbstractTableConfiguration
     protected function columns(): array
     {
         return [
-            // The table columns configuration.
+            Column::make('id'),
+            Column::make('blood_group_id'),
+            Column::make('breed_id'),
+            Column::make('breed'),
+            Column::make('pet_type_id'),
+            Column::make('avatar_id'),
+            Column::make('name'),
+            Column::make('age'),
+            Column::make('weight'),
+            Column::make('user_id'),
         ];
     }
 
